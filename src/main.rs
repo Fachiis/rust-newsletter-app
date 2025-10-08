16684496e6df81121cf581cd891886813b5709eb
Fachii Zasha <zashafachii@gmail.com>
2025-10-08 10:21:59 +0100

    Implement health check endpoint and set up proper testing system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+// Entry point of the application
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
+    let port = listener.local_addr()?.port();
+    println!("{}", format!("Server started at http://127.0.0.1:{}", port));
+    run(listener)?.await
 }

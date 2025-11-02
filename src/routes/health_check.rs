fdfd57cd04b691cdd02d2d6f6ee6644eff6ecf52
Fachii Zasha <zashafachii@gmail.com>
2025-11-02 23:12:55 +0100

    Simplify health check function to return a successful response without database query

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,20 +1,5 @@
-use actix_web::{web, HttpResponse};
-use sqlx::PgPool;
+use actix_web::HttpResponse;
 
-#[tracing::instrument(name = "Database health check", skip(pool))]
-pub async fn health_check(pool: web::Data<PgPool>) -> HttpResponse {
-    // HttpResponse::Ok().finish()
-    match sqlx::query!("SELECT 1 as check")
-        .fetch_one(pool.get_ref())
-        .await
-    {
-        Ok(_) => {
-            tracing::info!("Database connection successful");
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("Database connection failed: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+pub async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
